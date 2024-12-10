8245749ecd40c30ba5f24451f67cc3523d41e8e4
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2024-12-10 09:01:33 +0100

    format

--- HackerRank/Interview_Preparation_Kit/Warm_Up/Sales_by_Match/sales_by_match.py
+++ HackerRank/Interview_Preparation_Kit/Warm_Up/Sales_by_Match/sales_by_match.py
@@ -15,14 +15,14 @@ class SockMerchantTestCase(unittest.TestCase):
     def test_sock_merchant(self):
         ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
         self.assertEqual(sockMerchant(ar), 3)
-        
+
 
 def main():
     unittest.main()
 
 
 def run_hackerrank():
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     _ = int(input())
 
@@ -30,9 +30,10 @@ def run_hackerrank():
 
     result = sockMerchant(ar)
 
-    fptr.write(str(result) + '\n')
+    fptr.write(str(result) + "\n")
 
     fptr.close()
+
 
 if __name__ == "__main__":
     if os.environ.get("OUTPUT_PATH", None) is None:
